fcb608d446caf84433ac59117d624fea8657137b
kaxique <18671168+kaxique@users.noreply.github.com>
2025-03-23 02:51:19 +0100

    Use dynamically calculated width for nodes in graph

--- addons/goddagger/internal/view/graph/dependency_graph_panel.gd
+++ addons/goddagger/internal/view/graph/dependency_graph_panel.gd
@@ -1,3 +1,4 @@
+@tool
 class_name DependencyGraphPanel extends RefCounted
 
 
@@ -6,7 +7,7 @@ var _graph_nodes: Dictionary[String, ObjectGraphNode] = {}
 
 
 func _init(graph_canvas: GraphEdit) -> void:
-	self._graph_canvas = graph_canvas
+	_graph_canvas = graph_canvas
 
 
 func _connect_graph_nodes(
@@ -24,52 +25,20 @@ func draw_graph_for_component(
 ) -> void:
 	
 	var object_graph_layout := ObjectGraphLayout.new(component)
-	
-	var committed_width := 0
-	var committed_height := 0
-	var computing_width := 0
-	var computing_height := 0
-	var horizontal_padding := 60
-	var vertical_padding := 30
-	
 	var real_layers_amount := object_graph_layout.get_layers().size()
-	var total_layers_amount := real_layers_amount
 	
-	if total_layers_amount % 2 != 0:
-		total_layers_amount += 1
-	
-	var layout_sizes: Array[int] = []
-	layout_sizes.resize(total_layers_amount)
-	
-	for layer_number in total_layers_amount:
-		computing_width = 124 + horizontal_padding
-		
+	for layer_number in real_layers_amount:
 		var layer_visible_dependencies := [] if layer_number >= real_layers_amount \
 			else object_graph_layout.get_layers()[layer_number].get_visible_dependencies()
 		
 		for object in layer_visible_dependencies:
-			var object_node_view := ObjectGraphNode.spawn(object.get_name())
+			var object_node_view := ObjectGraphNode.spawn(object)
 			_graph_nodes[object.get_name()] = object_node_view
+			
+			if not component is GodDaggerParsingResult.CompiledResult.Subcomponent:
+				object_node_view.selected = true
 		
-		var total_dependencies_amount := 0 if layer_number >= real_layers_amount \
-			else object_graph_layout.get_layers()[layer_number].get_total_dependencies_amount()
-		var total_cross_layer_links_amount := 0 if layer_number >= real_layers_amount \
-			else object_graph_layout.get_layers()[layer_number].get_total_cross_layer_links_amount()
-		
-		var layer_vertical_size = total_dependencies_amount + total_cross_layer_links_amount
-		
-		if layer_vertical_size % 2 != 0:
-			layer_vertical_size += 1
-		
-		layout_sizes[layer_number] = layer_vertical_size
-		
-		computing_height = (102 + vertical_padding) * layer_vertical_size
-		
-		committed_width += computing_width
-		committed_height = max(computing_height, committed_height)
-		
-		computing_width = 0
-		computing_height = 0
+			_graph_canvas.add_child(object_node_view)
 	
 	for layer_number in real_layers_amount - 1:
 		var layer := object_graph_layout.get_layers()[layer_number]
@@ -88,6 +57,63 @@ func draw_graph_for_component(
 			for object in next_layer.get_visible_dependencies():
 				if dependency in object.get_dependencies():
 					_connect_graph_nodes(object, dependency)
+	
+	_graph_canvas.call_deferred("do_draw_object_graph_layout", object_graph_layout)
+
+
+func do_draw_object_graph_layout(object_graph_layout: ObjectGraphLayout) -> void:
+	var real_layers_amount := object_graph_layout.get_layers().size()
+	var total_layers_amount := real_layers_amount
+	
+	if total_layers_amount % 2 != 0:
+		total_layers_amount += 1
+	
+	var layout_sizes: Array[int] = []
+	layout_sizes.resize(total_layers_amount)
+	
+	var committed_width := 0
+	var committed_height := 0
+	var computing_width := 0
+	var computing_height := 0
+	var horizontal_padding := 60
+	var vertical_padding := 30
+	
+	for layer_number in total_layers_amount:
+		var layer_visible_dependencies := [] if layer_number >= real_layers_amount \
+			else object_graph_layout.get_layers()[layer_number].get_visible_dependencies()
+		
+		for object in layer_visible_dependencies:
+			var object_node_width := _graph_nodes[object.get_name()].get_size().x
+			computing_width = max(object_node_width, computing_width)
+		
+		var layer_hidden_dependencies := [] if layer_number >= real_layers_amount \
+			else object_graph_layout.get_layers()[layer_number].get_hidden_dependencies()
+		
+		for object in layer_hidden_dependencies:
+			var object_node_width := _graph_nodes[object.get_name()].get_size().x
+			computing_width = max(object_node_width, computing_width)
+		
+		var total_dependencies_amount := 0 if layer_number >= real_layers_amount \
+			else object_graph_layout.get_layers()[layer_number].get_total_dependencies_amount()
+		var total_cross_layer_links_amount := 0 if layer_number >= real_layers_amount \
+			else object_graph_layout.get_layers()[layer_number].get_total_cross_layer_links_amount()
+		
+		var layer_vertical_size = total_dependencies_amount + total_cross_layer_links_amount
+		
+		if layer_vertical_size % 2 != 0:
+			layer_vertical_size += 1
+		
+		layout_sizes[layer_number] = layer_vertical_size
+		
+		computing_width += horizontal_padding
+		committed_width += computing_width
+		
+		# TODO get rid of this hardcoded 102, use dynamically calculated value instead
+		computing_height = (102 + vertical_padding) * layer_vertical_size
+		committed_height = max(computing_height, committed_height)
+		
+		computing_width -= horizontal_padding
+		computing_height = 0
 	
 	for layer_number in layout_sizes.size():
 		var layer_vertical_size := layout_sizes[layer_number]
@@ -116,8 +142,7 @@ func draw_graph_for_component(
 						(committed_width / total_layers_amount) / 2,
 						0,
 					)
-				
-				_graph_canvas.add_child(object_node_view)
+				# TODO also centralize this link_node_view within the layer vertical bounds.
 			
 			elif link_index < layer_cross_links.size():
 				var dependency := layer_cross_links[link_index].get_dependency()
@@ -136,6 +161,7 @@ func draw_graph_for_component(
 						(committed_width / total_layers_amount) / 2,
 						0,
 					)
+				# TODO also centralize this link_node_view within the layer vertical bounds.
 				
 				var object := layer_cross_links[link_index].get_object()
 				var object_node_view := _graph_nodes[object.get_name()]
